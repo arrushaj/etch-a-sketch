62bc2cc4b964a00955353b50013bad56524f5c81
arrushaj <arditrrushaj@gmail.com>
2022-07-21 13:35:43 +0200

    Add button for generating custom size sketch pad

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,47 @@
 const grid = document.createElement('div');
+
+const button = document.createElement('button');
+button.textContent = "Generate sketch pad";
+button.addEventListener('click', createSketchPad);
+
 const container = document.querySelector('.container');
 
-grid.classList.add('grid');
+const row = document.createElement('div');
+row.classList.add('header');
+container.appendChild(row);
+row.appendChild(button);
+
+
+function createSketchPad() {
+    let squares;
+    do {
+        squares = prompt("How many squares per side? (max: 100)");
+    } while (squares > 100);
+    removeAllChildNodes(container);
+    const row = document.createElement('div');
+    row.classList.add('header');
+    container.appendChild(row);
+    row.appendChild(button);
+    for (i = 0; i < squares; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for (j = 0; j < squares; j++) {
+            const grid = document.createElement('div');
+            grid.addEventListener('mouseover', () => {
+                grid.classList.add('colored');
+            });   
+            grid.classList.add('grid');
+            row.appendChild(grid);
+        }
+    }
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
 for (i = 0; i < 16; i++) {
     const row = document.createElement('div');
@@ -11,10 +51,7 @@ for (i = 0; i < 16; i++) {
         const grid = document.createElement('div');
         grid.addEventListener('mouseover', () => {
             grid.classList.add('colored');
-        });
-        grid.addEventListener('mouseout', () => {
-            grid.classList.remove('colored');
-        });         
+        });   
         grid.classList.add('grid');
         row.appendChild(grid);
     }
